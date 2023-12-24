fc5faee3d4a6ced3a8b33c51dcae4916b4854ebd
Yavuz Çelik <63187562+y4wwy@users.noreply.github.com>
2023-12-24 14:23:44 +0300

    ...

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 "use strict";
+
+/**
+ * Light and dark mode
+ */
+
+const /** {NodeElement} */ $themeBtn = document.querySelector("[data-theme-btn]");
+const /** {NodeElement} */ $HTML = document.documentElement;
+let /** {Boolean | String} */ isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+if (sessionStorage.getItem("theme")) {
+  $HTML.dataset.theme = sessionStorage.getItem("theme");
+} else {
+  $HTML.dataset.theme = isDark ? "dark" : "light";
+}
+
+const changeTheme = () => {
+
+  $HTML.dataset.theme = sessionStorage.getItem("theme") === "light" ? "dark" : "light";
+  sessionStorage.setItem("theme", $HTML.dataset.theme);
+
+}
+
+$themeBtn.addEventListener("click", changeTheme);
