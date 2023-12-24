d7d39180e3a17c195138bc67039a41f82e56c0a1
Yavuz Çelik <63187562+y4wwy@users.noreply.github.com>
2023-12-24 14:24:04 +0300

    ...

--- js/script.js
+++ js/script.js
@@ -22,3 +22,27 @@ const changeTheme = () => {
 }
 
 $themeBtn.addEventListener("click", changeTheme);
+
+/**
+ * TAB
+ */
+
+const /** {NodeList} */ $tabBtn = document.querySelectorAll("[data-tab-btn]");
+let /** {NodeElement} */[lastActiveTab] = document.querySelectorAll("[data-tab-content]");
+let /** {NodeElement} */[lastActiveTabBtn] = $tabBtn;
+
+$tabBtn.forEach(item => {
+  item.addEventListener("click", function () {
+
+    lastActiveTab.classList.remove("active");
+    lastActiveTabBtn.classList.remove("active");
+
+    const /** {NodeElement} */ $tabContent = document.querySelector(`[data-tab-content="${item.dataset.tabBtn}"]`);
+    $tabContent.classList.add("active");
+    this.classList.add("active");
+
+    lastActiveTab = $tabContent;
+    lastActiveTabBtn = this;
+
+  });
+});
